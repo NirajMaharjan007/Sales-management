668beb56c3808d92051ae84058fd55c997d75ff4
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-10-12 14:43:16 +0545

    Updated
    ADDED

---
+++ front-end/src/app/categories/manage/manage.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CategoryManageComponent } from './manage.component';
+
+describe('CategoryManageComponent', () => {
+  let component: CategoryManageComponent;
+  let fixture: ComponentFixture<CategoryManageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CategoryManageComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CategoryManageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
