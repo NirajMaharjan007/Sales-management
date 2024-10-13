9de923715073e992c537eb08bff67c916f38192b
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-10-13 20:53:15 +0545

    Updated

--- front-end/src/app/product/create/create.component.ts
+++ front-end/src/app/product/create/create.component.ts
@@ -1,10 +1,20 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgFor],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css',
 })
-export class ProductCreateComponent {}
+export class ProductCreateComponent {
+  cards: { title: string }[] = [{ title: 'Card 1' }];
+  addCard(): void {
+    const newCardIndex = this.cards.length + 1;
+    this.cards.push({
+      title: `Card ${newCardIndex}`,
+    });
+  }
+}
