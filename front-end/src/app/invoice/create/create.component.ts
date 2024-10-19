8649c263b7a81f7859d142a8ab1b53fccc5ab18c
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-10-19 17:39:35 +0545

    Updated
    Little updated

--- front-end/src/app/invoice/create/create.component.ts
+++ front-end/src/app/invoice/create/create.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'invoice-create',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor, NgIf],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate('300ms ease-out', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class InvoiceCreateComponent {}
