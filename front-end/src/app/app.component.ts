c3d14e600c27d8a90fdef69f905902f978838c0d
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-10-22 21:06:26 +0545

    updated
    little
    
    Dated: oct-22 9:06PM

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -32,37 +32,37 @@ export class AppComponent implements OnInit {
       .subscribe(() => {
         if (this.router.url.startsWith('/home')) {
           this.htmlContent =
-            '<div class="d-flex justify-content-around align-items-center"> \
+            '<div class="d-flex justify-content-evenly align-items-center"> \
             <i class="fas fa-tachometer-alt fa-fw me-2" aria-current="true"> </i> \
             <span> Dashboard </span></div>';
         } else if (this.router.url.startsWith('/tax')) {
           this.htmlContent =
-            '<div class="d-flex justify-content-around align-items-center"> \
+            '<div class="d-flex justify-content-evenly align-items-center"> \
             <i class="fas fa-percent fa-fw me-2" aria-current="true"> </i> \
             <span> Tax Management </span></div>';
         } else if (this.router.url.startsWith('/categories')) {
           this.htmlContent =
-            '<div class="d-flex justify-content-around align-items-center"> \
+            '<div class="d-flex justify-content-evenly align-items-center"> \
             <i class="fas fa-th fa-fw me-2" aria-current="true"> </i> \
-            <span> Categories Management </span></div>';
+            <span> Categories</span></div>';
         } else if (this.router.url.startsWith('/products')) {
           this.htmlContent =
-            '<div class="d-flex justify-content-around align-items-center"> \
+            '<div class="d-flex justify-content-evenly align-items-center"> \
             <i class="fas fa-cube fa-fw me-2" aria-current="true"> </i> \
             <span> Products Management </span></div>';
         } else if (this.router.url.startsWith('/units')) {
           this.htmlContent =
-            '<div class="d-flex justify-content-around align-items-center"> \
+            '<div class="d-flex justify-content-evenly align-items-center"> \
             <i class="fas fa-balance-scale fa-fw me-2" aria-current="true"> </i> \
             <span> Units Management </span></div>';
         } else if (this.router.url.startsWith('/invoices')) {
           this.htmlContent =
-            '<div class="d-flex justify-content-around align-items-center"> \
+            '<div class="d-flex justify-content-evenly align-items-center"> \
             <i class="fas fa-file-invoice fa-fw me-2" aria-current="true"></i><span>Invoice</span> \
             </div>';
         } else if (this.router.url.startsWith('/sales')) {
           this.htmlContent =
-            '<div class="d-flex justify-content-around align-items-center"> \
+            '<div class="d-flex justify-content-evenly align-items-center"> \
           <i class="fas fa-dollar fa-fw me-2"></i><span>Sales</span> \
             </div>';
         } else {
