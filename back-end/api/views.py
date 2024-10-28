13d0452db3d9d99b1f2699fbfdf7b3ccbbb75b26
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-10-28 18:51:05 +0545

    Updated
    TODO:FIXes
    
    Redone

--- back-end/api/views.py
+++ back-end/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework import permissions
 from django.contrib import auth
 from django.contrib.auth.models import User
 from rest_framework import status
@@ -79,7 +79,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(AllowAny),
+    permission_classes=[permissions.AllowAny],
 )
 
 
