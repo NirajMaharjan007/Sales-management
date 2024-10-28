a5b3936d0111ec0fe3652a21b767608d731371af
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-10-28 14:33:06 +0545

    Updated
    Updated on navbar added logout

--- front-end/src/app/navbar/navbar.component.ts
+++ front-end/src/app/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
 import { UserService } from '../services/user.service';
 import { NgFor, NgIf } from '@angular/common';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -16,12 +18,26 @@ import { NgFor, NgIf } from '@angular/common';
 export class NavbarComponent implements OnInit {
   @Input() content: string = '';
 
+  router = inject(Router);
   user: any;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authServer: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.user = this.userService.getUser();
     // console.log(this.user);
+  }
+
+  logout(): void {
+    if (this.authServer.logout()) {
+      this.router.navigate(['/login']);
+      console.log('Logged out');
+    } else {
+      this.router.navigate(['/404']);
+      console.error('Error in logout');
+    }
   }
 }
