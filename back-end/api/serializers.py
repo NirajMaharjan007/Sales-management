2d2c3adf0969dae22098121784701d8347e3f981
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-10-28 18:45:51 +0545

    Updated
    TODO:FIXes

--- back-end/api/serializers.py
+++ back-end/api/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
 
+from base.models import Tax
+
 
 class UserSerializer(ModelSerializer):
     class Meta(object):
         model = User
         exclude = ['password']
+
+
+class TaxSerializer(ModelSerializer):
+    class Meta(object):
+        model = Tax
+        field = '__all__'
--- back-end/base/models.py
+++ back-end/base/models.py
@@ -1,6 +1,10 @@
-from django.db.models import Model, CharField, EmailField
+from django.db.models import Model, CharField, IntegerField
 
 
 # class User(Model):
 #     name = CharField(max_length=100)
 #     email = EmailField(unique=True)
+
+
+class Tax(Model):
+    tax = IntegerField()
