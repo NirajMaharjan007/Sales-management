d92bc1d18907b0bbdd6d63bcb4345510402c8fc1
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-10-31 12:16:44 +0545

    Updated API on backend

--- back-end/api/urls.py
+++ back-end/api/urls.py
@@ -1,34 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register(r'user', views.UserViewSet, basename='user')
+router.register(r'tax', views.TaxViewSet, basename='tax')
+router.register(r'category', views.CategoryViewSet, basename='category')
 
 
 urlpatterns = [
-    path('api/login', views.login, name='user_login'),
-    path('api/logout', views.logout, name='user_logout'),
-    path('api/signup', views.singup, name='user_signup'),
-    path('api/get_users', views.get_users, name='user_details'),
-    path('api/get_user_by_id/<int:id>/', views.get_user_by_id,
-         name='user_details_by_id'),
-
-    path('api/get_taxes', views.get_taxes, name='get_taxes'),
-    path('api/set_tax', views.set_tax, name='set_tax'),
-    path('api/delete_tax/<int:id>/', views.delete_tax, name='delete_tax'),
-
-    path('api/set_category', views.set_category, name='set_category'),
-    path('api/get_categories', views.get_categories, name='get_categories'),
-    path('api/delete_category/<int:id>/', views.delete_category,
-         name='delete_category'),
-    path('api/edit_category/<int:id>', views.updated_category,
-         name='updated_category'),
-    path('api/get_category/<int:id>/', views.get_category_by_id,
-         name='get_category_by_id'),
-
-    path('api/', views.schema_view.with_ui('swagger'),
+    path('swagger/', views.schema_view.with_ui('swagger'),
          name='schema-swagger-ui'),
     path('redoc/', views.schema_view.with_ui('redoc'),
          name='schema-redoc'),
 
-
-
-    # path('api/logout/', views.Logout(), name='logout'),
+    path('api/', include(router.urls)),
 ]
--- back-end/api/views.py
+++ back-end/api/views.py
@@ -1,73 +1,16 @@
-from random import randint
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import permissions
 from django.contrib import auth
 from django.contrib.auth.models import User
 from rest_framework import status
-# from django.http import JsonResponse
+from rest_framework.viewsets import ViewSet
 from rest_framework.authtoken.models import Token
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
 from .serializers import *
-
-
-# class UserView(APIView):
-@api_view(['GET'])
-def get_users(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-def get_user_by_id(request, id: int):
-    try:
-        user = User.objects.get(id=id)
-        serializer = UserSerializer(user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    except User.DoesNotExist:
-        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-    except Exception as e:
-        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-@api_view(['POST'])
-def login(request):
-    user = get_object_or_404(User, email=request.data['email'])
-    if not user.check_password(request.data['password']):
-        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
-    token, created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
-    auth.login(request, user)
-    return Response({"token": token.key, "user": serializer.data})
-
-
-@api_view(['POST'])
-def singup(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        user = User.objects.get(email=request.data['email'])
-        user.set_password(request.data['password'])
-        user.save()
-        token = Token.objects.create(user=user)
-        return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@ api_view(['POST'])
-def logout(request):
-    if hasattr(request.user, 'authtoken_token'):
-        request.user.auth_token.delete()
-        request.session.flush()
-        auth.logout(request=request)
-
-    return Response({'message': 'Logout successful'}, status=status.HTTP_200_OK)
 
 
 schema_view = get_schema_view(
@@ -84,83 +27,113 @@ schema_view = get_schema_view(
 )
 
 
-@api_view(['GET'])
-def get_taxes(request):
-    taxes = Tax.objects.all()
-    serializer = TaxSerializer(taxes, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['POST'])
-def set_tax(request):
-    serializer = TaxSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['DELETE'])
-def delete_tax(request, id: int):
-    try:
-        tax = Tax.objects.get(id=id)
-        tax.delete()
-        return Response({"message": "Tax deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
-    except Tax.DoesNotExist:
-        return Response({"error": "Tax not found"}, status=status.HTTP_404_NOT_FOUND)
-
-
-@api_view(['POST'])
-def set_category(request):
-    serializer = CategorySerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET'])
-def get_categories(request):
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-def get_category_by_id(request, id: int):
-    try:
-        category = Category.objects.get(id=id)
-        serializer = CategorySerializer(category)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    except Category.DoesNotExist:
-        return Response({"error": "Category not found"}, status=status.HTTP_404_NOT_FOUND)
-    except Exception as e:
-        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-@api_view(['DELETE'])
-def delete_category(request, id: int):
-    try:
-        category = Category.objects.get(id=id)
-        category.delete()
-        return Response({"message": "Category deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
-    except Tax.DoesNotExist:
-        return Response({"error": "Category not found"}, status=status.HTTP_404_NOT_FOUND)
-
-
-@api_view(['PATCH'])
-def updated_category(request, id: int):
-    try:
-        category = Category.objects.get(pk=id)
-    except Category.DoesNotExist:
-        return Response({'error': 'Category not found.'}, status=status.HTTP_404_NOT_FOUND)
-
-    serializer = CategorySerializer(category, data=request.data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
+class UserViewSet(ViewSet):
+    def list(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def retrieve(self, request, pk=None):
+        try:
+            user = User.objects.get(id=pk)
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    @action(detail=False, methods=['post'])
+    def signup(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(request.data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        user = get_object_or_404(User, email=request.data['email'])
+        if not user.check_password(request.data['password']):
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+        token, _ = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(instance=user)
+        auth.login(request, user)
+        return Response({"token": token.key, "user": serializer.data})
+
+    # Logout endpoint
+    @action(detail=False, methods=['post'])
+    def logout(self, request):
+        if hasattr(request.user, 'auth_token'):
+            request.user.auth_token.delete()
+            request.session.flush()
+            auth.logout(request=request)
+            return Response({'message': 'Logout successful'}, status=status.HTTP_200_OK)
+        return Response({'error': 'User is not logged in'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaxViewSet(ViewSet):
+    def list(self, request):
+        taxes = Tax.objects.all()
+        serializer = TaxSerializer(taxes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = TaxSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        try:
+            tax = Tax.objects.get(id=pk)
+            tax.delete()
+            return Response({"message": "Tax deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        except Tax.DoesNotExist:
+            return Response({"error": "Tax not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class CategoryViewSet(ViewSet):
+    def list(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        try:
+            category = Category.objects.get(id=pk)
+            serializer = CategorySerializer(category)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Category.DoesNotExist:
+            return Response({"error": "Category not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk=None):
+        try:
+            category = Category.objects.get(id=pk)
+            category.delete()
+            return Response({"message": "Category deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        except Category.DoesNotExist:
+            return Response({"error": "Category not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def partial_update(self, request, pk=None):
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            return Response({'error': 'Category not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CategorySerializer(
+            category, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
