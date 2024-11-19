36fe44cc7c4306c6f533d69d1051f03a2c53f28a
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-11-19 15:12:48 +0545

    Little Updated

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { SuppilersService } from '../services/suppliers.service';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: 0 })), // Initial state
+      transition(':enter', [animate('300ms ease-in')]), // Enter transition
+      transition(':leave', [animate('300ms ease-out')]), // Leave transition
+    ]),
+  ],
 })
 export class HomeComponent {
+  product_count = 0;
+  supplier_count = 0;
+
+  constructor(
+    private productsService: ProductsService,
+    private suppilersService: SuppilersService
+  ) {}
+  ngOnInit(): void {
+    this.fetch();
+  }
+
+  fetch() {
+    this.productsService.getProducts().subscribe((products) => {
+      this.product_count = products.length;
+    });
+
+    this.suppilersService.getSuppliers().subscribe((suppliers) => {
+      this.supplier_count = suppliers.length;
+    });
+  }
 }
