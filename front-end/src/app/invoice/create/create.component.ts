55a7dac6c8e0c4ae995b868f9406eaee705e04dd
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-11-21 00:53:48 +0545

    Updated
    Little changed
    
    TODO: FIX

--- front-end/src/app/invoice/create/create.component.ts
+++ front-end/src/app/invoice/create/create.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { animate, style, transition, trigger } from '@angular/animations';
 import { NgFor, NgIf } from '@angular/common';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'invoice-create',
@@ -19,7 +20,29 @@ import { NgFor, NgIf } from '@angular/common';
     ]),
   ],
 })
-export class InvoiceCreateComponent {
+export class InvoiceCreateComponent implements OnInit {
+  products: any;
+  price = 0;
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.fetch();
+  }
+
+  fetch() {
+    this.productsService.getProducts().subscribe((data) => {
+      this.products = data.map((product: ProductData) => {
+        return {
+          id: product.id,
+          name: product.name,
+          sales_price: product.sales_price,
+          qty: product.qty,
+        };
+      });
+    });
+  }
+
   cards = [{ id: 'card 1' }];
   addCard() {
     this.cards.push({ id: 'card' + this.cards.length });
@@ -32,4 +55,24 @@ export class InvoiceCreateComponent {
   hasMoreCard(): boolean {
     return this.cards.length > 1;
   }
+
+  click() {
+    console.info('onSubmit; ');
+  }
+
+  onProductChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    const productId = parseInt(target.value);
+    const product = this.products.find(
+      (product: ProductData) => product.id === productId
+    );
+    this.price = product?.sales_price || 0;
+  }
+}
+
+interface ProductData {
+  id: number;
+  name: string;
+  qty: number;
+  sales_price: number;
 }
