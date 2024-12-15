3323d98c9ceba349e34abbfbee96d78bd42fed5d
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2024-12-15 21:20:25 +0545

    FIXED
    todo: add backend & update frontend

--- front-end/src/app/settings/settings.component.ts
+++ front-end/src/app/settings/settings.component.ts
@@ -1,20 +1,23 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css',
 })
 export class SettingsComponent implements OnInit {
   user: any;
-
+  form: FormGroup;
   router = inject(Router);
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private fb: FormBuilder) {
+    this.form = this.fb.group({});
+  }
 
   ngOnInit(): void {
     try {
@@ -27,5 +30,9 @@ export class SettingsComponent implements OnInit {
 
   private fetch() {
     this.user = this.userService.getUser();
+  }
+
+  submit() {
+    console.log('GG');
   }
 }
